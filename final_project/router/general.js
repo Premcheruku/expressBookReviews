3175411419205e28731131b99a188601675f8b11
harsha <harshavardhanreddycheruku@gmail.cl.com>
2026-09-09 02:38:55 -0400

    Complete Task 2 get book by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,16 +39,9 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-
-  if (!books[isbn]) {
-    return res.status(404).json({
-      message: "Book not found"
-    });
-  }
-
-  return res.status(200).json(books[isbn]);
-});
+    const isbn = req.params.isbn;
+    res.send(JSON.stringify(books[isbn], null, 2));
+  });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
