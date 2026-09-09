fb5120ae8dbb2b47c84f03c04937743dd2fc2986
harsha <harshavardhanreddycheruku@gmail.cl.com>
2026-09-09 03:10:11 -0400

    Complete Task 4 get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -61,22 +61,21 @@ public_users.get('/author/:author', function (req, res) {
   });
 
 // Get all books based on title
+
 public_users.get('/title/:title', function (req, res) {
-  const title = req.params.title;
-
-  const result = Object.values(books).filter(
-    book => book.title.toLowerCase() === title.toLowerCase()
-  );
-
-  if (result.length === 0) {
-    return res.status(404).json({
-      message: "No books found with this title."
+    const title = req.params.title;
+    let result = [];
+  
+    let keys = Object.keys(books);
+  
+    keys.forEach((isbn) => {
+      if (books[isbn].title === title) {
+        result.push(books[isbn]);
+      }
     });
-  }
-
-  return res.status(200).json(result);
-});
-
+  
+    res.send(JSON.stringify(result, null, 2));
+  });
 // Get book review
 public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
